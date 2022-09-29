68fad40c18822b2f5574bd5e487309f3591217f5
Patricia Robiola <patorobiola@gmail.com>
2022-09-29 19:41:12 -0300

    Subiendo Proyecto Final

---
+++ ProyectoFinal.js
@@ -0,0 +1,6 @@
+/*function saludo() {
+    var nombre = prompt('Ingrese su nombre por favor');
+    alert(`Hola ${nombre}, que disfrutes tu paseo a través de mis ojos por San Nicolás Aventura`);
+}
+
+saludo();*/
